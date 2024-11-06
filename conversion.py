43cdf33e02bf51c4bcb5e06ed063b7831d245858
Joshua Lawrimore <josh.lawrimore@gmail.com>
2024-11-06 10:53:09 -0500

    Corrected typo in README.md file. Added bulk label addition to conversion.py script. Want to update this to allow for each region to be its own subgroup.

--- conversion.py
+++ conversion.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from typing import Generator
 
 import dask.array as da
+import pandas as pd
+import numpy as np
 import tifffile as tf
 import zarr
 from ome_zarr.io import parse_url
@@ -12,9 +14,10 @@ STACKS_ROOT: Path = Path(
     r"data/210810_45670_ko_female_LH_14-48-50_decon_2021-10-28_12-39-11"
 )
 IMAGE_SUBDIR: Path = STACKS_ROOT.joinpath(r"640_N4")
-ATLAS_SUBDIR: Path = STACKS_ROOT.joinpath(r"atlaslabel_df_origspace")
+ATLAS_SUBDIR: Path = STACKS_ROOT.joinpath(r"atlaslabel_def_origspace")
 SEGMENTATION_SUBDIR: Path = STACKS_ROOT.joinpath(r"640_FRST_seg")
 STORE: Path = STACKS_ROOT.joinpath(STACKS_ROOT.name + ".zarr")
+ATLAS_COLOR_MAP: Path = Path(r"atlas_info_v3.csv")
 
 # Process the N4 deconned images as the primary images in the zarr directory
 store = parse_url(STORE, mode="w").store
@@ -61,3 +64,48 @@ root.attrs["omero"] = {
         }
     ]
 }
+del total_array
+
+# labels section
+# convert labels CSV into dict
+atlas_images: Generator = ATLAS_SUBDIR.rglob("*.tif")
+sorted_atlas_images: list = sorted(list(atlas_images))
+with tf.TiffFile(sorted_atlas_images[0]) as atlas_tif:
+    atlas_y_dim, atlas_x_dim = atlas_tif.pages[0].shape
+print("Creating the Atlas dask array...")
+atlas_array = da.zeros(
+    (len(sorted_atlas_images), atlas_y_dim, atlas_x_dim), dtype=da.uint16
+)
+unique_atlas_values = set()
+for i, atlas_image in tqdm(
+    enumerate(sorted_atlas_images), total=len(sorted_atlas_images)
+):
+    with tf.TiffFile(atlas_image) as atlas_tif:
+        atlas_tif_array = atlas_tif.asarray()
+        unique_values = set(np.unique(atlas_tif_array).astype(int))
+        unique_atlas_values.update(unique_values)
+        atlas_array[i, :, :] = atlas_tif_array
+labels_grp = root.create_group("labels")
+label_name = "atlas_regions"
+labels_grp.attrs["labels"] = [label_name]
+label_grp = labels_grp.create_group(label_name)
+# create dictionary containing a list of dictionaries
+# that assigns rgba color for region id value
+atlas_df = pd.read_csv(ATLAS_COLOR_MAP)
+mapped_colors = atlas_df["id"].unique()
+missing_colors = list(unique_atlas_values - set(mapped_colors.astype(int)))
+missing_colors = [int(x) for x in missing_colors if x > 0]
+
+colors_list = []
+for _, row in atlas_df.iterrows():
+    color_dict = {
+        "label_value": int(row["id"]),
+        "rgba": [row["red"], row["green"], row["blue"], 255],
+    }
+    colors_list.append(color_dict)
+for missing_color in missing_colors:
+    missing_color_dict = {"label_value": missing_color, "rgba": [0, 0, 0, 255]}
+    colors_list.append(missing_color_dict)
+atlas_labels_dict = {"colors": colors_list}
+label_grp.attrs["image-label"] = atlas_labels_dict
+write_image(atlas_array, label_grp, axes="zyx")
